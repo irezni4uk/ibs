a19c1d62e8f50dadd6e3818d9abecaa9371a409f
Ilya <ilya@gake.com>
2019-09-27 17:22:38 +0300

    friday upload

--- ibs/InternalBallisticsSimulator.go
+++ ibs/InternalBallisticsSimulator.go
@@ -26,14 +26,6 @@ type SimParams struct {
 func (i *InternalBallisticsSimulator) RunSym() float64 {
 	i.Reset()
 
-	// fmt.Println("Running golang interior ballistics simulation, got following inputs:")
-	// fmt.Println(fmt.Sprintf("%#v", i.Barrel))
-	// fmt.Println(fmt.Sprintf("%#v", i.Projectile))
-	// fmt.Println(fmt.Sprintf("%#v", i.Charge))
-	// fmt.Println(fmt.Sprintf("%#v", i.Volume()))
-	// fmt.Println(fmt.Sprintf("%#v", i.Barrel.Area()))
-	// fmt.Println(fmt.Sprintf("%#v", i.Barrel.FrictionFactor()))
-
 	// n := 0
 	t := 0.
 	for t < wallTime && i.Projectile.Path < i.Barrel.Length {
@@ -41,17 +33,14 @@ func (i *InternalBallisticsSimulator) RunSym() float64 {
 		s.Time = t
 		i.State(&s)
 		i.Step(&s)
-		fmt.Printf("%+v\n", s)
-		// fmt.Println(fmt.Sprintf("%#v", s))
-		// fmt.Println(i.Projectile.Velocity, i.Charge.HeatCapacity(), i.Charge.GasMass(), i.Volume(), 1/2)
-		// fmt.Println(fmt.Sprintf("t: %.1f ms\tPmean: %.0f MPa\tPbase: %.0f MPa\tTgas: %.0f K\tCp: %.0f J/K\trho: %.1f kg/m3\tV: %.0f m/s\tx: %.3f m", t*1e3, Pmean/1e6, Pbase/1e6, Tgas, i.Charge.HeatCapacity(), i.Charge.GasMass()/i.Volume(), i.Projectile.Velocity, i.Projectile.Path))
+		// fmt.Printf("%+v\n", s)
 
 		t += dt
 	}
-	fmt.Println(fmt.Sprintf("Eproj: %.0f J, Eprop: %.0f J, Q: %.0f J",
-		i.Projectile.KineticEnergy(),
-		i.Charge.KineticEnergy(i.Projectile.Velocity),
-		i.Barrel.Q))
+	// fmt.Println(fmt.Sprintf("Eproj: %.0f J, Eprop: %.0f J, Q: %.0f J",
+	// 	i.Projectile.KineticEnergy(),
+	// 	i.Charge.KineticEnergy(i.Projectile.Velocity),
+	// 	i.Barrel.Q))
 	return 42
 }
 
@@ -92,6 +81,9 @@ func (i *InternalBallisticsSimulator) LinkComponents() {
 	i.Params = &SimParams{i.Projectile.Mass, i.Charge.Mass(), i.Barrel.BoreArea}
 	i.Barrel.Sp = i.Params
 }
+
+// func (i *InternalBallisticsSimulator) String() out string {
+// }
 
 // func (i *InternalBallisticsSimulator) dudt(u float64) float64 {
 // 	i.Propellant.Z = u
